aaf7925cd48f5fe3dcbbfc59d920587511ce52af
Aslanbek Kaipaev <kaipaeff@gmail.com>
2024-09-03 23:24:18 +0300

    Dev (#3)
    * create repository
    
    * vite, ESLint, Prettier installed
    
    * release_0. Base structure and styles added

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <div className="container">
+      <h1 className="chart-title">Delta Charts</h1>
+      <table className="table-body">
+        <tr className="table-head_row">
+          <th className="table-head_title">Показатель</th>
+          <th className="table-head_title">Текущий день</th>
+          <th className="table-head_title">Вчера</th>
+          <th className="table-head_title">Этот день недели</th>
+        </tr>
+      </table>
+    </div>
+  );
 }
 
-export default App
+export default App;
