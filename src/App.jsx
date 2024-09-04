19a3fcee14f33c7a94594524a464d2a446b49bcb
Aslanbek <kaipaeff@gmail.com>
2024-09-04 11:33:51 +0300

    table structure done

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,40 @@
+import data from '../db.json';
+
 function App() {
+  const { indicators, current_day, last_day, this_day_week } = data;
+
+  const handleClick = id => {
+    console.log(id);
+  };
+
   return (
     <div className="container">
       <h1 className="chart-title">Delta Charts</h1>
       <table className="table-body">
-        <tr className="table-head_row">
-          <th className="table-head_title">Показатель</th>
-          <th className="table-head_title">Текущий день</th>
-          <th className="table-head_title">Вчера</th>
-          <th className="table-head_title">Этот день недели</th>
-        </tr>
+        <thead>
+          <tr className="table-head_row">
+            <th className="table-head_title head-title">Показатель</th>
+            <th className="table-head_title">Текущий день</th>
+            <th className="table-head_title">Вчера</th>
+            <th className="table-head_title">Этот день недели</th>
+          </tr>
+        </thead>
+        <tbody>
+          {indicators.map(indicator => {
+            const currentValue = current_day.find(item => item.id === indicator.id)?.value || '-';
+            const lastValue = last_day.find(item => item.id === indicator.id)?.value || '-';
+            const weekValue = this_day_week.find(item => item.id === indicator.id)?.value || '-';
+
+            return (
+              <tr key={indicator.id} className="table-head_row" onClick={() => handleClick(indicator.id)}>
+                <td className="table-body_data data-title">{indicator.title}</td>
+                <td className="table-body_data">{currentValue}</td>
+                <td className="table-body_data">{lastValue}</td>
+                <td className="table-body_data">{weekValue}</td>
+              </tr>
+            );
+          })}
+        </tbody>
       </table>
     </div>
   );
